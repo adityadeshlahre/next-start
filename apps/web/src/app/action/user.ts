19ff2b7d7c4cc257573828b7792aedd6f2f19005
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-12-04 10:50:52 +0530

    fix:database migration test out 00

--- apps/web/src/app/action/user.ts
+++ apps/web/src/app/action/user.ts
@@ -10,6 +10,7 @@ export async function signup(
   const user = await prisma.user.create({
     data: {
       name: username,
+      username: username,
       email: email,
       password: password,
     },
